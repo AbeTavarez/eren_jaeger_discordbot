04f7b1ab97dc78a8339c71488d62b616dbf20cb3
Abraham E. Tavarez <abetavarez@icloud.com>
2020-12-22 22:31:06 -0500

    initial commit

--- eren_jaeger_bot.py
+++ eren_jaeger_bot.py
@@ -6,4 +6,4 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}.format(client)')
+    print('We have logged in as {0.user}'.format(client))
