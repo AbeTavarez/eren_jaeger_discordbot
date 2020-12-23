a30c9d8c5703374ad74c5550294cd464e64e626c
Abraham E. Tavarez <abetavarez@icloud.com>
2020-12-22 22:38:12 -0500

    basic bot

--- eren_jaeger_bot.py
+++ eren_jaeger_bot.py
@@ -1,9 +1,24 @@
 import discord
+import os
 
 
 client = discord.Client()
+
+# @event : as soon as the bot is ready
 
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(msg):
+    if msg.author == client.user:
+        return
+
+    if msg.content.startswith('$hello'):
+        await msg.channel.send('Hello!')
+
+
+client.run(os.getenv('TOKEN'))
