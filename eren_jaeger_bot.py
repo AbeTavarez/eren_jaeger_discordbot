3eae81b086292c0353ede98dc0d204c7848946a8
Abraham E. Tavarez <abetavarez@icloud.com>
2020-12-23 10:40:39 -0500

    fetching data and returning it to chat

--- eren_jaeger_bot.py
+++ eren_jaeger_bot.py
@@ -1,9 +1,17 @@
+import json
+import requests
 import discord
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
 client = discord.Client()
+
+
+def get_quote():
+    res = requests.get("http://zenquotes.io/api/random")
+    json_data = json.loads(res.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]["a"]
+    return(quote)
 
 
 @client.event  # @event: as soon as the bot is ready
@@ -16,8 +24,9 @@ async def on_message(msg):
     if msg.author == client.user:
         return
 
-    if msg.content.startswith('$hello'):
-        await msg.channel.send('Hello!')
+    if msg.content.startswith('$tatakae'):
+        quote = get_quote()
+        await msg.channel.send(quote)
 
 
 client.run(os.getenv('TOKEN'))
